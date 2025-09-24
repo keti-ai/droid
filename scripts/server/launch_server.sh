39d02d569adc3a477a57ede2b3dbb21c56fe5402
syh4661 <syh20131292@gmail.com>
2025-09-24 22:34:11 +0900

    Fix runtime: export GXX before conda activate to avoid unbound variable

--- scripts/server/launch_server.sh
+++ scripts/server/launch_server.sh
@@ -7,11 +7,12 @@ if [ -f ~/miniconda3/etc/profile.d/conda.sh ]; then
 elif [ -f ~/anaconda3/etc/profile.d/conda.sh ]; then
   source ~/anaconda3/etc/profile.d/conda.sh
 fi
+# Some conda activation scripts (gxx) expect GXX to be set when -u is enabled
+export GXX=${GXX:-$HOME/miniconda3/envs/polymetis-local/bin/x86_64-conda-linux-gnu-c++}
 conda activate polymetis-local
 
 export CONDA_PREFIX=${CONDA_PREFIX:-$HOME/miniconda3/envs/polymetis-local}
 export LD_LIBRARY_PATH="$CONDA_PREFIX/lib:$LD_LIBRARY_PATH"
-export GXX=${GXX:-$CONDA_PREFIX/bin/x86_64-conda-linux-gnu-c++}
 
 cd $( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 python run_server.py
