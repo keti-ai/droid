0a3d506655ea869ccd0bf79829424112e90ed674
syh4661 <syh20131292@gmail.com>
2025-09-24 22:33:30 +0900

    Stabilize build/run: pin protobuf/grpc to 3.18/1.41, remove gxx activation errors, set GXX in runtime

--- scripts/server/launch_server.sh
+++ scripts/server/launch_server.sh
@@ -11,6 +11,7 @@ conda activate polymetis-local
 
 export CONDA_PREFIX=${CONDA_PREFIX:-$HOME/miniconda3/envs/polymetis-local}
 export LD_LIBRARY_PATH="$CONDA_PREFIX/lib:$LD_LIBRARY_PATH"
+export GXX=${GXX:-$CONDA_PREFIX/bin/x86_64-conda-linux-gnu-c++}
 
 cd $( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 python run_server.py
